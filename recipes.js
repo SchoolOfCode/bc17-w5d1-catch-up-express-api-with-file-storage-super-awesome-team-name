01ed0cbb64cdfa45139ee63d0e03e514fe869114
BrishaP <152220142+BrishaP@users.noreply.github.com>
2024-07-29 11:03:27 +0100

    beginning to import recipes json file

--- recipes.js
+++ recipes.js
@@ -1,10 +1,13 @@
 import fs from "node:fs/promises";
 import { v4 as uuidv4 } from "uuid";
-
+//STEP 1: IMPORT JSON RECIPES FILE
+import recipes from "/Users/brishapatel/Documents/SoC/Week 5/bc17-w5d1-catch-up-express-api-with-file-storage-super-awesome-team-name/recipes.json";
 const fileName = "recipes.json";
 
 // GET ALL RECIPES
-export async function getRecipes() {}
+export async function getRecipes() {
+//RETURN RECIPES; 
+}
 
 // GET A RECIPE BY ID
 export async function getRecipeByID(id) {}
