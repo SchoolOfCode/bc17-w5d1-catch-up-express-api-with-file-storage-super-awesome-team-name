92dd8bc861efbef01ea65a071bb74604d800cdd2
Liban Yusuf <lyus01306@gmail.com>
2024-07-29 11:57:35 +0100

    Created helper function for getrecipe

--- app.js
+++ app.js
@@ -10,9 +10,43 @@ import {
 
 const app = express();
 const PORT = 3000;
-
 app.use(express.static("public"));
 app.use(express.json());
+
+
+//GETHANDLER for Recipies
+app.get('/api/recipes',async (req,res) =>{
+try{
+  //Fetching Data using the helper Function
+  const recipes = await getRecipes()
+  res.status.json(200)({
+    Success : True ,
+    Payload : recipes});
+  }catch (error){
+      res.status.json(400)({
+        Sucess : false,
+        Payload :null
+      })
+    }
+  })
+
+
+
+
+
+// //app.get('/astronauts', async (req, res) => {
+//   try {
+//     const astronauts = await getAstronauts(); //// Fetch data using the helper function
+//     res.json({success: true, payload: astronauts}); //Send successful JSON response
+//   } catch (error) {
+//     res.status(500).json({ success: false, payload: error.message }); // Handle errors
+//   }
+// }
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
--- recipes.js
+++ recipes.js
@@ -1,12 +1,13 @@
 import fs from "node:fs/promises";
 import { v4 as uuidv4 } from "uuid";
 //STEP 1: IMPORT JSON RECIPES FILE
-import recipes from "/Users/brishapatel/Documents/SoC/Week 5/bc17-w5d1-catch-up-express-api-with-file-storage-super-awesome-team-name/recipes.json";
+import recipes from "./recipes.json"  assert { type: `json` }; 
 const fileName = "recipes.json";
 
 // GET ALL RECIPES
 export async function getRecipes() {
 //RETURN RECIPES; 
+return recipes;
 }
 
 // GET A RECIPE BY ID
